10905204dde201943d2e0e728b1eb88ecd899097
sindre <sindre.heggen@gmail.com>
2023-04-18 09:45:16 +0200

    division minigames release

--- minigames/div/div_of_amounts/div_of_amounts.gd
+++ minigames/div/div_of_amounts/div_of_amounts.gd
@@ -3,7 +3,7 @@ extends MiniGame
 const Number = preload("res://minigames/div/div_of_amounts/number.gd")
 var one_texture : Texture2D = preload("res://minigames/div/div_of_amounts/assets/one.svg")
 var ten_texture : Texture2D = preload("res://minigames/div/div_of_amounts/assets/ten.svg")
-var divisor : int = 5
+var divisor : int = 3
 var quotient : int 
 var remainder : int
 var group_container := HBoxContainer.new()
@@ -14,41 +14,50 @@ var start_position : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	super()
 	window = get_viewport_rect().size
 	start_position = Vector2(window.x/2, 500)
 		
-	var dividend_group : Area2D = preload("res://minigames/div/div_of_amounts/group.tscn").instantiate()
+	var dividend_group : Area2D = preload("res://minigames/div/div_of_amounts/total_group.tscn").instantiate()
 	dividend_group.position = Vector2(window.x/2, start_position.y-25)	
-	dividend_group._arrange(Vector2(600, 330))
+	dividend_group._arrange(Vector2(1000, 330))
+
 	add_child(dividend_group)
 	
 	group_container.set("theme_override_constants/separation", 350)
 	group_container.set_alignment(group_container.ALIGNMENT_CENTER)
 	group_container.size = Vector2(window.x, 800)
 	group_container.position.y = start_position.y + 400
+
+	divisor = randi() % 4 + 2
 	for i in range(divisor):
 		var cntrl := Control.new()
 		var new_group: Area2D = preload("res://minigames/div/div_of_amounts/group.tscn").instantiate()
-		new_group._arrange(Vector2(300, 330))
+		new_group._arrange(Vector2(310, 330))
 		cntrl.add_child(new_group)
 		group_container.add_child(cntrl)
 	add_child(group_container)
-	
 	_mk_number()
+
 
 func _mk_number() -> void:
 	randomize()
 	quotient = (randi() % 10 + 1)
-	var dividend : int = 45 #quotient*divisor 	
+#	var dividend : int = 45 #quotient*divisor 	
 	
 	@warning_ignore("integer_division")
-	var tens : int = dividend / 10
-	var ones : int = dividend - 10*tens 
+#	var tens : int = dividend / 10
+#	var ones : int = dividend - 10*tens 
+	var tens : int = randi_range(0, (10/divisor))
+	@warning_ignore("integer_division")
+	var ones : int = randi_range(1, (10/divisor)-1)
+	tens = tens*divisor
+	ones = ones*divisor
 	
 	var texture_size = one_texture.get_size()
 	for i in range(ones):
 		var one := Number.new()
-		one.position = start_position + Vector2(50, i*texture_size.y - 20)
+		one.position = start_position + Vector2(50, i*texture_size.y - 150)
 		one.value = 1
 		add_child(one)
 		one.button_texture = one_texture
--- minigames/div/divisor1to10/divisor1to10_loader.gd
+++ minigames/div/divisor1to10/divisor1to10_loader.gd
@@ -1,11 +1,12 @@
-extends Node2D
+extends MiniGame
 
-var game : Control = preload("res://minigames/div/divisor1to10/Divisor1To9.tscn").instantiate() 
+var game : Control = preload("res://minigames/div/divisor1to10/divisor1to10.tscn").instantiate() 
 var hbox := HBoxContainer.new()
 const DivisorButton = preload("res://minigames/div/divisor1to10/divisor_button.gd")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	super()
 	hbox.set("theme_override_constants/separation", 100)
 	for i in range(1, 11):
 		var button := DivisorButton.new()
