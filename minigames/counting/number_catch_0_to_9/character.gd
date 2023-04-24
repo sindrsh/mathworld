a5fa6181b5fc2970116be88dfe2bb22b1a1dcc75
@racascou <rafaelcastrocouto@gmail.com>
2023-04-24 11:34:31 +0200

    number catch juice

--- minigames/counting/number_catch_0_to_9/character.gd
+++ minigames/counting/number_catch_0_to_9/character.gd
@@ -1,8 +1,8 @@
-extends Area2D
+extends Node2D
 
 var max_speed := 800
 var value := 1
-
+var swing = 0
 
 func _ready():
 	position = Vector2(get_viewport_rect().size.x/2, get_viewport_rect().size.y - 100) 
@@ -24,8 +24,35 @@ func _mouse_input(delta) -> Vector2:
 	else:
 		return mouse_position
 
-func _physics_process(delta):
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		position.x = _mouse_input(delta).x
+
+func _move_and_rotate(x):
+	var a = get_node('number_container').rotation
+	var max_a = 0.02
+	var speed_a = 0.004
+	var d = x - position.x
+	if (d > 0 and a < max_a):
+		_rotate_childs(speed_a)
+	if (d < 0 and a > -max_a):
+		_rotate_childs(-speed_a)
 	else:
-		position.x = _keyboard_input(delta).x
+		_rotate_childs(sin(swing)*0.0005)
+	return x
+
+
+func _rotate_childs(speed_a):
+	var node = self
+	var a = speed_a
+	for n in value:
+		if (node.has_node('number_container')): 
+			node = node.get_node('number_container')
+			node.rotation += a
+
+func _physics_process(delta):
+	swing += 0.04
+	var x
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		x = _mouse_input(delta).x
+	else:
+		x = _keyboard_input(delta).x
+	
+	position.x = _move_and_rotate(x)
