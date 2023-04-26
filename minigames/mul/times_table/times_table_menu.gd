c11d428ad0d068fd7f31c2b1a08b7f98e2dd5342
@racascou <rafaelcastrocouto@gmail.com>
2023-04-26 11:35:35 +0200

    menu buttons

--- minigames/mul/times_table/times_table_menu.gd
+++ minigames/mul/times_table/times_table_menu.gd
@@ -1,14 +1,20 @@
-extends HBoxContainer
+extends MiniGame
 
-var minigame_choice : Button = preload("res://mini_game_choice.tscn").instantiate()
+var minigame_choice : TextureButton = preload("res://mini_game_choice.tscn").instantiate()
 var dir_body : String = "res://minigames/mul/times_table/Times"
 var numbers : PackedStringArray = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	minigame_choice.custom_minimum_size = Vector2(100,50)
+func _add_generics():
+	var margin = MarginContainer.new()
+	add_child(margin)
+	
+	var container = GridContainer.new()
+	margin.add_child(container)
+	container.columns = 5
+	
 	for i in range(numbers.size()):
 		var game_button = minigame_choice.duplicate()
-		game_button.text = str(i)
+		game_button.get_node('Label').text = str(i)
 		game_button.scene_path = dir_body + numbers[i] + ".tscn"
-		add_child(game_button)
+		container.add_child(game_button)
