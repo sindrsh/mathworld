fd0a7e9b88707ca35f13dd0bb56854b8cf468e74
@racascou <rafaelcastrocouto@gmail.com>
2023-05-19 10:26:54 -0300

    hsl shader, success message

--- minigames/generics/success_message.gd
+++ minigames/generics/success_message.gd
@@ -1,14 +1,14 @@
-extends Node2D
+extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	assert($ToMenuButton.connect("pressed", _go_to_menu) == 0)
-	$Background.size = get_viewport_rect().size
-	$ToMenuButton.position = get_viewport_rect().size/2
-	$Label.position = get_viewport_rect().size/2 - Vector2(0,100)
+	assert($"%ToMenuButton".connect("pressed", _go_to_menu) == 0)
+	assert($"%RestartButton".connect("pressed", _restart) == 0)
 
 
 func _go_to_menu() -> void:
 	if get_tree().change_scene_to_file("res://start_menu.tscn") != OK:
 		print("Could not change scene")
 
+func _restart() -> void:
+	get_tree().reload_current_scene()
