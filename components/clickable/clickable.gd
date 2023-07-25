1f15ce4eb1ef15d4e9bdc80c3f2c8f3a455087ef
firesquid <jdeiss06@gmail.com>
2023-07-25 16:56:51 -0500

    add clickable component

---
+++ components/clickable/clickable.gd
@@ -0,0 +1,31 @@
+extends Area2D
+# emits the signal on_click when clicked on
+class_name Clickable
+
+# TODO: implement support for press and release (cool UI effects?)
+
+var hovering = false
+signal on_click
+signal on_hover_start
+signal on_hover_stop
+
+
+
+
+func _on_mouse_entered() -> void:
+	hovering = true;
+	emit_signal("on_hover_start")
+
+
+func _on_mouse_exited() -> void:
+	hovering = false
+	emit_signal("on_hover_stop")
+
+
+func _input(event) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed and hovering:
+			emit_signal("on_click")	
+		return;
+
+	# TODO: make this work when the screen is tapped as well
