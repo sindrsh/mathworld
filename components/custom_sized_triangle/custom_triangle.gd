28ffa4baf104b01be4e5a7b743e228f4a1e59c87
firesquid <jdeiss06@gmail.com>
2023-08-03 16:26:33 -0500

    add triangle

--- components/custom_sized_triangle/custom_triangle.gd
+++ components/custom_sized_triangle/custom_triangle.gd
@@ -1,11 +1,41 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var triangle: Polygon2D = get_node("Triangle")
+
+# index 0 = a, index 1 = b 
+var bases = [50, 50]
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# actually updates the triangle's position
+func update_triangle():
+	# the "a" side should be situated on the x axis with its midpoint at 0 on the x axis
+	# the "b" side should be situated on the y axis with tis midpoint at 0 on the y axis
+	# Therefore, the three ts of the triangle will be at:
+	# (0.5 * a, -0.5 * b)
+	# (-0.5 * a, -0.5 * b)
+	# (0.5 * a, 0.5 * b)
+	# this should keep the triangle centered
+	
+	# had to draw this out on paper to figure it out lol
+	triangle.polygon.clear()
+	triangle.polygon.append(Vector2(0.5 * bases[0], -0.5 * bases[1]))
+	triangle.polygon.append(Vector2(-0.5 * bases[0], -0.5 * bases[1]))
+	triangle.polygon.append(Vector2(0.5 * bases[0], 0.5 * bases[1]))
+	
+
+
+# if the bases need to be changed that can just be done by manually setting the value
+# in the array
+# hypo = hypotaneuse or whatever I can't spell it
+# will change the base not specified by fix
+func change_hypo(c, fix="a"):
+	var changing_base = 1
+	var constant_base = 0
+	if fix == "b":
+		changing_base = 0
+		constant_base = 1
+	
+	# pythagorean theorem in reverse
+	# a = sqrt(c^2 - b^2)
+	bases[changing_base] = sqrt((c * c) - (bases[constant_base] * bases[constant_base]))
---
+++ minigames/pyt/pyt_puzzle2/pyt.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# a
+func _on_h_slider_value_changed(value):
+	pass # Replace with function body.
+
+
+# b
+func _on_h_slider_2_value_changed(value):
+	pass # Replace with function body.
+
+
+# c
+func _on_h_slider_3_value_changed(value):
+	pass # Replace with function body.
