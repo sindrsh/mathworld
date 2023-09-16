d07fb0614c9f2e44dbf42eff9342c72d08c157ee
sindre <sindre.heggen@gmail.com>
2023-09-16 22:52:21 +0200

    run_hill

--- minigames/generics/run_hill/run_hill.gd
+++ minigames/generics/run_hill/run_hill.gd
@@ -1,38 +1,69 @@
 extends MiniGame
 
+var speed := 100.0
+var amplitude := 100.0
+var graph := Line2D.new()
+var tick_texture : Texture2D = preload("res://minigames/counting/falling_numbers_0_to_9/assets/tick_slct.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
-	var n := 100
+	var n := 200
 	var dx := 2*PI/n
-	var graph_container := Node2D.new()
+	
+	graph.position = Vector2(400, 500)
+	$Path2D.position = graph.position
+	graph.default_color = Color(0, 0, 1)
+	
+	for i in range(n):
+		var point := amplitude*Vector2(dx*i, -_path_func(dx*i))
+		graph.add_point(point)
+		$Path2D.curve.add_point(point + Vector2(0, -32))
+		
+	add_child(graph)
+	_add_ticks()
+
+func _physics_process(delta):
+	pass
+#	$Path2D/PathFollow2D.set_progress($Path2D/PathFollow2D.get_progress() + speed * delta)
+
+
+func _path_func(x: float) -> float:
+	return cos(x)
+
+func _path_func_der(x: float) -> float:
+	return sin(x)
+
+func _add_ticks() -> void:
+	var n := 2000
+	var dx := 2*PI/n
+	var graph_points = PackedVector2Array()
+	var distance := 0.0
+	var distances := [0.0]
+	graph_points.append(amplitude*Vector2(0,-1))
 	
 	for i in range(1, n+1):
-		var road_block := StaticBody2D.new()
-		var road_block_collision := CollisionShape2D.new()
-		var road_block_shape := SegmentShape2D.new()
-		var a := 100*Vector2(dx*(i-1), -cos(dx*(i-1)))
-		var b := 100*Vector2(dx*i, -cos(dx*i))	
-		road_block_collision.shape = road_block_shape
-		
-		road_block_shape.a = a
-		road_block_shape.b = b
-		
-		var segment := Line2D.new()
-		segment.default_color = Color(0, 0, 1)
-		segment.points = PackedVector2Array(
-			[
-			a,
-			b	
-			]
-		)
-		
-		graph_container.add_child(segment)
-		road_block.add_child(road_block_collision)
-		graph_container.add_child(road_block)
-	graph_container.position = Vector2(100, 200)
-	add_child(graph_container)
+		graph_points.append(amplitude*Vector2(dx*i, -_path_func(dx*i)))
+		distance += graph_points[i-1].distance_to(graph_points[i])
+		distances.append(distance)
 	
-
-
+	var ds = distance/10.0
+	var i := 0
+	
+	for j in range(distances.size()):
+		if distances[j] >= i*ds:
+			var tick := Sprite2D.new()
+			tick.texture = tick_texture
+			var slope_angle := Vector2(1, _path_func_der(graph_points[j].x)).angle()
+			if i < 5:
+				slope_angle = abs(slope_angle)
+			else: 
+				slope_angle = -abs(slope_angle)
+			tick.rotate(slope_angle)
+			tick.position = graph.position + graph_points[j]
+			
+			i += 1
+			add_child(tick)
+		
+		
+			
