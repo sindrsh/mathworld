bfa52a3cde278277034cf72c3bb9c36c1dc73d79
sindre <sindre.heggen@gmail.com>
2023-10-13 13:12:11 +0200

    added music

--- minigames/counting/amount_0_to_9/amount_0_to_9.gd
+++ minigames/counting/amount_0_to_9/amount_0_to_9.gd
@@ -2,6 +2,8 @@ extends  "res://minigames/generics/puzzles/amounts/place_position.gd"
 
 #explanation
 
+var music : AudioStreamMP3 = preload("res://minigames/generics/assets/little-slime.mp3")
+
 func _add_specifics() -> void:
 	world_part = "counting"
 	id = "amount_0_to_9"
@@ -12,7 +14,8 @@ func _add_specifics() -> void:
 	
 	_add_number(1)
 	_connect_line(number_boards[1], number_places[1])
-
+	
+	music_player.stream = music
 
 func _end_game_condition() -> bool:
 	return number_boards[1].value == 9
