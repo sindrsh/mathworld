a79b046c9d77b24c2ff1e650b48c7a3d37a6b7a1
firesquid6 <jdeiss06@gmail.com>
2023-12-16 17:43:18 -0600

    add slide

--- components/ending_slide/ending_slide.gd
+++ components/ending_slide/ending_slide.gd
@@ -1,11 +1,16 @@
 extends Node2D
+class_name EndingSlide
+
+# Note: this node will not work properly unless is at (0, 0)
+# It also makes the assumption that there is no camera
+# if a camera is implemented later, contact @firesquid6 and I'll help deal with that
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# causes the slide in animation to start
+# put stars as the number of stars to display
+func slide_in(stars: int):
+	animation_player.play("slide_in")
+	print("I'm sliding in")
