38bbe760b057e83f2ff9fb9e82ec5cf3455c5d27
firesquid6 <jdeiss06@gmail.com>
2023-12-16 17:54:08 -0600

    add ending_slide component

--- components/ending_slide/ending_slide.gd
+++ components/ending_slide/ending_slide.gd
@@ -5,12 +5,34 @@ class_name EndingSlide
 # It also makes the assumption that there is no camera
 # if a camera is implemented later, contact @firesquid6 and I'll help deal with that
 
-
+@onready var increase_timer: Timer = get_node("IncreaseTimer")
 @onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
+@onready var sprite: AnimatedSprite2D = get_node("AnimatedSprite2D")
+signal animation_complete
+
+var stars: int = -1
+
+func _ready():
+	# code just for testing
+	if get_parent().name == "root":
+		slide_in(3)
 
 
 # causes the slide in animation to start
-# put stars as the number of stars to display
-func slide_in(stars: int):
+# put s as the number of stars to display
+func slide_in(s: int):
+	stars = s
 	animation_player.play("slide_in")
 	print("I'm sliding in")
+
+
+func _on_animation_player_animation_finished(anim_name):
+	increase_timer.start()
+
+
+func _on_increase_timer_timeout():
+	if sprite.frame < stars:
+		increase_timer.start()
+		sprite.frame += 1
+	else:
+		emit_signal("animation_complete")
