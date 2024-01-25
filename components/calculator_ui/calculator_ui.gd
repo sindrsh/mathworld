da0e5ebb2927801e11884c20e3e6d4124fe36182
firesquid6 <jdeiss06@gmail.com>
2024-01-25 17:41:54 -0600

    add theme to calculator UI

--- components/calculator_ui/calculator_ui.gd
+++ components/calculator_ui/calculator_ui.gd
@@ -7,35 +7,32 @@ var number_string = "";
 @onready var score_label: Label = get_node("Score");
 signal number_entered(number: int);
 
-
 func _on_number_pressed(number: String):
-  number_string += number;
-  label.text = number_string;
-
+	number_string += number;
+	label.text = number_string;
 
 func _on_enter():
-  emit_signal("number_entered", int(number_string));
-
-  _on_clear();
+	emit_signal("number_entered", int(number_string));
+	_on_clear();
 
 
 func _input(event):
-  if event is InputEventKey:
-	if event.pressed:
-	  if event.as_text_keycode() == "C":
-		_on_clear()
-	  elif event.as_text_keycode() == "Enter":
-		_on_enter()
+	if event is InputEventKey:
+		if event.pressed:
+			if event.as_text_keycode() == "C":
+				_on_clear()
+			elif event.as_text_keycode() == "Enter":
+				_on_enter()
 
 
 func _on_clear():
-  number_string = "";
-  label.text = number_string;
+	number_string = "";
+	label.text = number_string;
 
 
 func _on_shooter_score_changed(score: int):
-  score_label.text = "Score: " + str(score);
+	score_label.text = "Score: " + str(score);
 
 
 func _on_shooter_damange_taken(health: int):
-  health_label.text = "Health: " + str(health)  
+	health_label.text = "Health: " + str(health)  
