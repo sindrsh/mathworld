dbe36d7b1f97d291ccccdbf2a3084c13cc0875d2
firesquid6 <jdeiss06@gmail.com>
2024-03-04 21:57:14 -0600

    add input stack

--- components/input_stack.gd
+++ components/input_stack.gd
@@ -1,11 +1,23 @@
-extends Node
+extends Object
+class_name TimedInputQueue
+# technically, pop is O(n), not O(1). This isn't a leetcode problem, so it doesn't really matter
 
+var input_lifespan = 0.1  # lifespan of inputs in seconds. Inputs that are "older" than this number will be thrown out
+var _inputs: Array[String] = []
+var _times: Array[float] = []
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func push(input: String):
+	_inputs.append(input)
+	_times.append(Time.get_unix_time_from_system())
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# may return an empty string if the queue is empty
+func pop() -> String:
+	var currentTime = Time.get_unix_time_from_system()
+	while len(_times) > 0:
+		var time = _times.pop_front()
+		var input = _inputs.pop_front()
+		
+		if time + input_lifespan >= currentTime:
+			return input
+		
+	return ""
