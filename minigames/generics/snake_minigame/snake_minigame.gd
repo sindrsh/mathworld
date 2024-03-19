cdd9580a3a4e5249239c2a70a519afa2f4ca4b0f
firesquid6 <jdeiss06@gmail.com>
2024-03-18 21:11:16 -0500

    make spawn fruit it's own function

--- minigames/generics/snake_minigame/snake_minigame.gd
+++ minigames/generics/snake_minigame/snake_minigame.gd
@@ -6,12 +6,22 @@ var _fruit_scene = preload("res://components/fruit/fruit.tscn")
 
 
 func _add_specifics():
-	
 	world_part = "counting"
 	id = "snake_1_to_50" 
 	minigame_type = AMOUNT
 	
-	for i in range(1, 10):
+	spawn_fruit(1, 10)
+	
+	_add_status_bar()
+	
+func _physics_process(_delta) -> void:
+	if $Snake:
+		status_bar.frame = $Snake.health
+	
+
+
+func spawn_fruit(low: int, high: int):
+	for i in range(low, high):
 		# max x = 29
 		# max y = 14
 		var spawn_pos = Vector2(randi_range(1, 29), randi_range(1, 14)) * 64
@@ -22,10 +32,3 @@ func _add_specifics():
 		fruit.position = spawn_pos
 		
 		print("created fruit at " + str(spawn_pos))
-	
-	_add_status_bar()
-	
-func _physics_process(_delta) -> void:
-	if $Snake:
-		status_bar.frame = $Snake.health
-				
